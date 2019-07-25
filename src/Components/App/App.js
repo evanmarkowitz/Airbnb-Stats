398162eda644f09772e3c0e77c21965dd666755b
Evan Markowitz <emarkowitz1@gmail.com>
2019-07-25 13:24:43 -0600

    Build folder structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App =() => {
+  return (
+    <main className='app'>
+      <h1>AIRBNB PRICES BY NEIGHBORHOOD</h1>
+  
+    </main>
+  );
+}
+
+export default App;
