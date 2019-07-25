68925b5b633b90352cc402186faa5e7f753c6d9f
Evan Markowitz <emarkowitz1@gmail.com>
2019-07-25 13:59:01 -0600

    Made state change when borough or neighborhood clicked

--- src/Containers/Filter/Filter.js
+++ src/Containers/Filter/Filter.js
@@ -5,8 +5,13 @@ export class Filter extends Component {
   constructor() {
     super()
     this.state = {
-
+      roomType: '',
+      borough: ''
     }
+  }
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+    
   }
   render() {
     return (
@@ -14,16 +19,45 @@ export class Filter extends Component {
         <article>
           <h3>Type of apartment</h3>
           <div className='buttons'>
-            <button>Entire Apartment</button>
-            <button>Room</button>
+
+            <button onClick={(event) => this.handleChange(event)}
+            name='roomType'
+            value='entire_apartment'
+            className='filter-button'>Entire Apartment</button>
+
+            <button onClick={(event) => this.handleChange(event)}
+            name='roomType'
+            value='room'
+            className='filter-button'>Room</button>
+
           </div>
           <h3>Borough</h3>
           <div className='buttons'>
-            <button>Manhattan</button>
-            <button>Brooklyn</button>
-            <button>Queens</button>
-            <button>Bronx</button>
-            <button>Staten Island</button>
+            <button className='filter-button'
+            onClick={(event) => this.handleChange(event)}
+            name='borough'
+            value='Manhattan'
+            >Manhattan</button>
+            <button className='filter-button'
+            onClick={(event) => this.handleChange(event)}
+            name='borough'
+            value='Brooklyn'>
+            Brooklyn</button>
+            <button className='filter-button'
+            onClick={(event) => this.handleChange(event)}
+            name='borough'
+            value='Queens'>
+            Queens</button>
+            <button className='filter-button'
+            onClick={(event) => this.handleChange(event)}
+            name='borough'
+            value='Bronx'>
+            Bronx</button>
+            <button className='filter-button'
+            onClick={(event) => this.handleChange(event)}
+            name='borough'
+            value='Staten Island'>
+            Staten Island</button>
           </div>
         </article>
 
