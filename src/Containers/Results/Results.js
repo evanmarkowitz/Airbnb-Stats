b4378d9ccc836b8b8c7e5f5d24edd83a430ca5d4
Evan Markowitz <emarkowitz1@gmail.com>
2019-07-27 10:42:48 -0600

    Add Google Maps

--- src/Containers/Results/Results.js
+++ src/Containers/Results/Results.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 import './Results.css'
 
-export const Result = ({ apts, hood}) => {
+export const Result = ({ apts, hood, google}) => {
 
 
   return (
     <section className='results'>
       <p>{hood}</p>
-      <iframe className='map' src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBUjpj2w4odf15sphWyngSyGf8VLgPViy0&q=${hood},New+York,NY`}></iframe>
-
+      {/* <iframe className='map' src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBUjpj2w4odf15sphWyngSyGf8VLgPViy0&q=${hood},New+York,NY`}></iframe> */}
+      <Map
+          google={google}
+          zoom={8}
+          // style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
 
     </section>
   )
 }
 
-const mapStateToProps = (state) => ({
-  apts: state.apts,
-  hood: state.hood
-})
+// const mapStateToProps = (state) => ({
+//   apts: state.apts,
+//   hood: state.hood
+// })
 
-export default connect(mapStateToProps)(Result)
+// export default connect(mapStateToProps)(Result)
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBUjpj2w4odf15sphWyngSyGf8VLgPViy0'
+})(Result);
